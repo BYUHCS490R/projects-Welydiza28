f21d90230bd3a75183e4baef27976ab4b4398ac5
Samuel <swelyardiza28@gmail.com>
2025-11-19 21:41:52 -1000

    add formprocessing on projects

---
+++ projects/formprocessing/form.js
@@ -0,0 +1,49 @@
+document.getElementById("myForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const age = document.getElementById("age").value;
+
+        if (!name) {
+            alert("Please provide your name!");
+            return;
+        }
+
+        if (!email) {
+            alert("Please provide your email!");
+            return;
+        }
+
+        if (!password || password.length < 8) {
+            alert("Please enter a password with a minimum 8 characters!");
+            return;
+        }
+
+        if (!age || age < 1 || age > 100) {
+            alert("Please enter a valid age between 1 and 100!")
+            return;
+        }
+
+        const data = {
+            name: name,
+            email: email,
+            password: password,
+            age: age
+        }
+
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", "submit.json", true);
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.onreadystatechange = function () {
+            if(xhr.readyState === 4 && xhr.status === 200) {
+                const response = JSON.parse(xhr.responseText);
+                document.getElementById("message").innerHTML = response.message;
+                document.getElementById("myForm").innerHTML = "";
+            } else if (xhr.readyState === 4) {
+                alert('Error submitting form.');
+            }
+        };
+        xhr.send(JSON.stringify(data))
+        console.log(data);;
+    });
